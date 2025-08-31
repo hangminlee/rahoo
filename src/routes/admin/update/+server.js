fc8df22f27191699f72271afd08e9ce942973684
meanI-kor <happyapril24@naver.com>
2025-08-31 20:02:32 +0900

    test

--- src/routes/admin/update/+server.js
+++ src/routes/admin/update/+server.js
@@ -7,7 +7,7 @@ export async function POST () {
         {
             timeout: 60000
         },
-        (error, stdout, stderr) => {
+        await (error, stdout, stderr) => {
             console.log('stdout:',stdout);
             console.log('stderr:',stderr);
             if (error !== null) console.log('exec error:', error);
