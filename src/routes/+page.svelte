d0613e4162e57ffe52262ffa0693e5afec8b11be
meanI-kor <happyapril24@naver.com>
2025-08-31 20:45:11 +0900

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -141,7 +141,7 @@
     gap: 1em;
     align-items: center;
     justify-content: space-between;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(5, fit-content);
 }
 .menus ul li {
     display: block;
