397d61f30f16a05675ab73bc0da85795fd2046d5
meanI-kor <happyapril24@naver.com>
2025-08-31 19:59:12 +0900

    test

--- src/routes/admin/update/+server.js
+++ src/routes/admin/update/+server.js
@@ -2,11 +2,16 @@ import {json} from '@sveltejs/kit';
 import {exec} from 'child_process';
 
 export async function POST () {
-    let child = exec("pwd", (error, stdout, stderr) => {
-        console.log('stdout:',stdout);
-        console.log('stderr:',stderr);
-        if (error !== null) console.log('exec error:', error);
-    });
+    let child = await exec(
+        "pwd",
+        {
+            timeout: 60000
+        },
+        (error, stdout, stderr) => {
+            console.log('stdout:',stdout);
+            console.log('stderr:',stderr);
+            if (error !== null) console.log('exec error:', error);
+        });
     console.log(child);
 
     return json({data: 'eof'},{ status: 200});
