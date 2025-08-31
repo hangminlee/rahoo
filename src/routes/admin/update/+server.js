9ae450818f3443dcee21e4d0830585b0fcbc5c7d
meanI-kor <happyapril24@naver.com>
2025-08-31 20:00:03 +0900

    test

--- src/routes/admin/update/+server.js
+++ src/routes/admin/update/+server.js
@@ -12,7 +12,7 @@ export async function POST () {
             console.log('stderr:',stderr);
             if (error !== null) console.log('exec error:', error);
         });
-    console.log(child);
+    console.log('child:', child);
 
     return json({data: 'eof'},{ status: 200});
 }
