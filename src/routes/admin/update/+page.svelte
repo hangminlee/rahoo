8bbc3e934434c3468d29155b80d38b49a795726d
meanI-kor <happyapril24@naver.com>
2025-08-31 19:47:20 +0900

    update 기능

--- src/routes/admin/update/+page.svelte
+++ src/routes/admin/update/+page.svelte
@@ -1,1 +1,22 @@
-<div>업데이트 중...</div>
+<script>
+  import { onMount } from "svelte";
+
+    onMount(()=>{
+        fetch("./",{
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        });
+    })
+</script>
+<div class="sub">업데이트 중...</div>
+<style>
+    .sub {
+        display: flex;
+        width: 100%;
+        height: 100dvh;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/routes/admin/update/+server.js
@@ -0,0 +1,14 @@
+import {json} from '@sveltejs/kit';
+import {exec} from 'child_process';
+
+async function POST () {
+
+    let child = exec("pwd", (error, stdout, stderr) => {
+        console.log('stdout:',stdout);
+        console.log('stderr:',stderr);
+        if (error !== null) console.log('exec error:', error);
+    });
+    console.log(child);
+
+    return json({data: 'eof'},{ status: 200});
+}
