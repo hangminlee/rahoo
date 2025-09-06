751609746347e65f6b9c4549f0345f004ec84f46
hangminlee <happyapril24@gmail.com>
2025-09-06 10:33:11 +0900

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,7 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
     return {
-      title: 'RAFOO | 라멘 덕후들만을 위한 구르메-'
+        title: 'RAFOO | 라멘 덕후들만을 위한 구르메-',
+        slug: ''
     }
 }
